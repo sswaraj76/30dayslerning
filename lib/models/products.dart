b68a53c18372f14fd5c00ebc1449dcdbc38926fa
sswaraj76 <sswaraj413@gmail.com>
2021-02-13 12:22:04 +0530

    day-6 complete

---
+++ lib/models/products.dart
@@ -0,0 +1,21 @@
+class Items {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Items({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  Items(
+      id: "01",
+      name: "iPhone 12",
+      desc: "",
+      price: 59999,
+      color: "#FFA500",
+      image:
+          "https://rukminim1.flixcart.com/image/832/832/kg8avm80/mobile/r/h/z/apple-iphone-12-dummyapplefsn-original-imafwg8dqgncgbcb.jpeg?q=70")
+];
